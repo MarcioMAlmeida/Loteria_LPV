a371a7b7ae22f0bc6caee68209df08a070bd497d
MarcioMAlmeida <marcioa.marcioa@gmail.com>
2024-02-27 17:27:25 -0300

    terminando refatoração

--- src/Classe.java
+++ src/Classe.java
@@ -5,13 +5,20 @@ import java.util.Random;
 public class Classe {
 
     public double loteria(List<Integer> jogo, double premioTotal) {
+        int acertos;
+        double premioGanho;
+        List<Integer> jogoVencedor;
+        List<Integer> jogoValido = new ArrayList<>();
 
-        List<Integer> jogoVencedor = sorteiaNumeros();
-        
-        List<Integer> jogoValido = validaJogo(jogo);
+        jogoVencedor = sorteiaNumeros();
+        try {
+            jogoValido = validaJogo(jogo);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        int acertos = confereNumeros(jogoVencedor, jogoValido);
-        double premioGanho = calcularPremio(premioTotal, acertos);
+        acertos = confereNumeros(jogoVencedor, jogoValido);
+        premioGanho = calcularPremio(premioTotal, acertos);
 
         return premioGanho;
     }
@@ -28,14 +35,17 @@ public class Classe {
         return jogoVencedor;
     }
 
-    private static List<Integer> validaJogo(List<Integer> jogo) {
+    private static List<Integer> validaJogo(List<Integer> jogo) throws Exception {
         List<Integer> jogoValido = new ArrayList<>();
-        for (Integer n: jogo){
-            if (n < 1 || n > 60){
-                return null;
+        if (jogo.size() >= 6 && jogo.size() <= 15) {
+            throw new Exception("Tamanho do jogo inválido");
+        }
+        for (Integer n : jogo) {
+            if (n < 1 || n > 60) {
+                throw new Exception("Jogo contém número inválido");
             }
-            if (jogoValido.contains(n)){
-                return null;
+            if (jogoValido.contains(n)) {
+                throw new Exception("Jogo contém números repetidos");
             }
             jogoValido.add(n);
         }
@@ -44,22 +54,20 @@ public class Classe {
 
     private static int confereNumeros(List<Integer> jogoVencedor, List<Integer> jogoValido) {
         int acertos = 0;
-        if (jogoValido.size() >= 6 && jogoValido.size() <= 15) {
-            for (Integer i: jogoValido){
-                if (jogoVencedor.contains(i)){
-                    acertos++;
-                }
+        for (Integer i : jogoValido) {
+            if (jogoVencedor.contains(i)) {
+                acertos++;
             }
         }
         return acertos;
     }
 
     private static double calcularPremio(double premioTotal, int acertos) {
-        if (acertos == 6){
+        if (acertos == 6) {
             return premioTotal;
-        } else if (acertos == 5){
+        } else if (acertos == 5) {
             return premioTotal * 0.2;
-        } else if (acertos == 4){
+        } else if (acertos == 4) {
             return premioTotal * 0.05;
         }
         return 0.0;
